e7ae19cf1563aa59717167ee899d621b4328d6d8
annie_truex <artruex@gmail.com>
2020-05-20 11:49:16 -0700

    add logic to controllers

--- crafts-backend/app/controllers/crafts_controller.rb
+++ crafts-backend/app/controllers/crafts_controller.rb
@@ -1,2 +1,32 @@
 class CraftsController < ApplicationController
+    
+    def index
+        @crafts = Craft.all 
+        render json: @crafts
+    end
+
+    def create 
+        @craft = Craft.new(craft_params)
+        if @craft.save 
+            render json: @craft
+        else 
+            render json: {error: 'error creating your craft :('}
+        end
+        
+    end
+
+    def show 
+        @craft = Craft.find(params[:id])
+        render json: @craft 
+    end
+
+    def destroy
+        @craft = Craft.find(params[:id])
+        @craft.destroy 
+    end
+
+    private 
+    def craft_params
+        params.require(:craft).permit(:name, category)
+    end
 end
--- crafts-backend/app/controllers/materials_controller.rb
+++ crafts-backend/app/controllers/materials_controller.rb
@@ -1,2 +1,37 @@
 class MaterialsController < ApplicationController
+
+    before_action :set_craft
+
+    def index
+       @materials = @craft.materials 
+       render json: @materials
+    end
+
+    def create 
+        @material = @craft.material.new(material_params)
+        if @material.save 
+            render json: @material
+        else
+            render json: {error: 'error creating your material :('}
+        end
+    end
+
+    def show 
+        @material = @craft.find_by(id: params[:id])
+        render json: @material
+    end
+
+    def destroy
+        @material.destroy
+    end
+
+    private 
+
+    def set_craft 
+        @craft = Craft.find(params[:craft_id])
+    end
+
+    def material_params
+        params.require(:material).permit(:name, :craft_id, :description)
+    end
 end
