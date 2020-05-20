d8c6d56cd46bce88103b70f524552ae949c178b8
annie_truex <artruex@gmail.com>
2020-05-20 11:47:20 -0700

    add relationships and validations to models

--- crafts-backend/app/models/craft.rb
+++ crafts-backend/app/models/craft.rb
@@ -1,2 +1,6 @@
 class Craft < ApplicationRecord
+    has_many :materials 
+    validates :name, presence: true
+    
+    validates :name, presence: true
 end
--- crafts-backend/app/models/material.rb
+++ crafts-backend/app/models/material.rb
@@ -1,2 +1,5 @@
 class Material < ApplicationRecord
+    belongs_to :craft 
+
+    validates :name, presence: true
 end
