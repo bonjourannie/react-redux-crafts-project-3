4c9f87cd4475a80c7fb86d063a2806cce873decd
annie_truex <artruex@gmail.com>
2020-05-21 14:58:07 -0700

    create store in index.js

--- crafts-frontend/src/App.js
+++ crafts-frontend/src/App.js
@@ -1,22 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends React.Component{
-
-componentDidMount(){
-  fetch('http://localhost:3001/crafts', {
-    method: 'GET'
-  })
-  .then(resp => resp.json())
-  .then(resp => console.log(resp[0]))
-}
-
 
   render(){
     return(
       <div className="App">
         please show up
-        
+
       </div>
     )
   }
