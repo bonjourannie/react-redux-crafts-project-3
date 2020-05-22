1899770148fd99239f3c78501c946c97984a5ad3
annie_truex <artruex@gmail.com>
2020-05-22 12:54:22 -0700

    add logic to CraftList to return crafts

--- crafts-frontend/src/components/CraftList.js
+++ crafts-frontend/src/components/CraftList.js
@@ -1,12 +1,13 @@
 //functional component responsible for rendering list of crafts
 import React from 'react'
-import CraftInput from './CraftInput'
+// import CraftInput from './CraftInput'
+// import CraftsContainer from '../containers/CraftsContainer'
 
-const CraftList = ({props}) => {
+const CraftList = (props) => {
 
     return(
         <div>
-            CraftList
+            {props.crafts.map(craft => <li key={craft.id}> {craft.name} - {craft.category} - {craft.notes} </li>)}
         </div>
     )
 
