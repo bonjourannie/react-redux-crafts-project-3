f2c0da79a4021da9f15964d083a6a371d6896b3c
annie_truex <artruex@gmail.com>
2020-05-24 10:09:05 -0700

    add form to MaterialInput

--- crafts-frontend/src/components/MaterialInput.js
+++ crafts-frontend/src/components/MaterialInput.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 
 class MaterialInput extends Component {
 
     render(){
         return (
             <div>
-                MaterialInput
+                <form onSubmit={this.handleSumbut}>
+                    <label>Add materials to your craft</label>
+                    <br></br>
+                    <input type='text' placeholder='name' value ={this.state.name} name="name" onChange={this.handleChange}/>
+                    <br></br>
+                    <input type='text' placeholder='description' value={this.state.category} name="description" onChange={this.handleChange}/>
+                    <br></br>
+                    <input type="submit"/>
             </div>
         )
     }
@@ -13,7 +21,14 @@ class MaterialInput extends Component {
 
 }
 
-export default MaterialInput
+export default connect(null)(MaterialInput)
 
 //class component because it's a controlled form 
-//handling values & state
+//handling values & state, must connect to store
+//going to submit new material that will need 
+//to be sent to db, then dispatched to redux store
+//to update store. so connect component so when I call 
+//function to create new material, it dispatches action to reducer
+
+//doesn't need mapStateToProps because it gets props from Material Container
+//will need mapDispatch or something like that
