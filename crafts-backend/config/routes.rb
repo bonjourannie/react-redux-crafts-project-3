f0264250919757e7748f14f2c6ad1f218f9e186d
annie_truex <artruex@gmail.com>
2020-05-30 14:59:45 -0700

    add comments on backed

--- crafts-backend/config/routes.rb
+++ crafts-backend/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :comments
-  # resources :craft_logs
+ 
   resources :crafts  do
     resources :materials
+    resources :comments
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
